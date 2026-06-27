7fa32fbbc0f6e14d41a6937e16acc633a345121d
delgithub4 <103834452+delgithub4@users.noreply.github.com>
2026-06-27 18:36:34 +0100

    Create planner.py

---
+++ planner.py
@@ -0,0 +1,10 @@
+class Planner:
+
+    def create_plan(self, task):
+
+        return [
+            "Understand the task",
+            "Break task into steps",
+            "Execute each step",
+            "Return the result"
+        ]
