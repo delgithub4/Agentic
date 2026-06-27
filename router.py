c1197144c797429ff9b2cb737a91af747102f484
delgithub4 <103834452+delgithub4@users.noreply.github.com>
2026-06-27 18:49:49 +0100

    Create router.py

---
+++ router.py
@@ -0,0 +1,22 @@
+class Router:
+
+    def decide(self, task):
+
+        task = task.lower()
+
+        if "calculate" in task:
+            return "calculator"
+
+        if "time" in task:
+            return "time"
+
+        if "date" in task:
+            return "date"
+
+        if "write" in task:
+            return "writer"
+
+        if "research" in task:
+            return "research"
+
+        return "general"
