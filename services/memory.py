d0423436f5c373b60914ae7929a07f846842d488
delgithub4 <103834452+delgithub4@users.noreply.github.com>
2026-06-28 20:12:44 +0100

    Update memory.py

--- services/memory.py
+++ services/memory.py
@@ -1,1 +1,23 @@
+import json
+
+
+class Memory:
+
+    FILE = "data/memory.json"
+
+    def save(self, task):
+
+        try:
+
+            with open(self.FILE, "r") as file:
+                history = json.load(file)
+
+        except:
+
+            history = []
+
+        history.append(task)
+
+        with open(self.FILE, "w") as file:
+            json.dump(history, file, indent=4)
 
