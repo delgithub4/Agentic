f12b96bc92bf92459c44bc2269fcbf4f026d76ba
delgithub4 <103834452+delgithub4@users.noreply.github.com>
2026-06-29 22:02:38 +0100

    Create init.py

---
+++ interfaces/init.py
@@ -0,0 +1,4 @@
+from .agent_interface import AgentInterface
+from .tool_interface import ToolInterface
+from .workflow_interface import WorkflowInterface
+from .repository_interface import RepositoryInterface
