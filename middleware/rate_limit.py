a2506fa6508b6faf35f5f909e9abdb93d88ec91b
delgithub4 <103834452+delgithub4@users.noreply.github.com>
2026-06-29 22:10:57 +0100

    Create rate_limit.py

---
+++ middleware/rate_limit.py
@@ -0,0 +1,12 @@
+from starlette.middleware.base import BaseHTTPMiddleware
+
+
+class RateLimitMiddleware(BaseHTTPMiddleware):
+
+    async def dispatch(
+        self,
+        request,
+        call_next,
+    ):
+
+        return await call_next(request)
