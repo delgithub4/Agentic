b7d26d1c0fa21db15aed66f87f2702948438faaa
delgithub4 <103834452+delgithub4@users.noreply.github.com>
2026-06-29 22:04:32 +0100

    Create repository_interface.py

---
+++ interfaces/repository_interface.py
@@ -0,0 +1,12 @@
+from abc import ABC, abstractmethod
+
+
+class RepositoryInterface(ABC):
+
+    @abstractmethod
+    def save(self, item):
+        ...
+
+    @abstractmethod
+    def all(self):
+        ...
