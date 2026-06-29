e9f216de1bd9c477911da4220d04290967b81758
delgithub4 <103834452+delgithub4@users.noreply.github.com>
2026-06-29 21:55:56 +0100

    Create init.py

---
+++ managers/init.py
@@ -0,0 +1,5 @@
+from .agent_manager import AgentManager
+from .planning_manager import PlanningManager
+from .task_manager import TaskManager
+from .tool_manager import ToolManager
+from .workflow_manager import WorkflowManager
